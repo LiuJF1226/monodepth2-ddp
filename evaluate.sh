47afaa62bc7b6c2049070843c4c2b63a9d77aa0c
Sauf4896 <34789037+Sauf4896@users.noreply.github.com>
2023-10-24 10:19:46 +0800

    Update evaluate.sh

--- evaluate.sh
+++ evaluate.sh
@@ -5,9 +5,9 @@ CUDA_VISIBLE_DEVICES=0 python evaluate_depth.py \
 --kitti_path /home/datasets/kitti_raw_data \
 --make3d_path /home/datasets/make3d \
 --cityscapes_path /home/datasets/cityscapes \
---nyuv2_path /home/datasets/nyu_v2 
-# --use_stereo
-# --post_process
+--nyuv2_path /home/datasets/nyu_v2 \
+# --use_stereo \
+# --post_process \
 
 
 CUDA_VISIBLE_DEVICES=0 python evaluate_pose.py \
@@ -15,5 +15,6 @@ CUDA_VISIBLE_DEVICES=0 python evaluate_pose.py \
 --data_path /home/datasets/kitti_odometry \
 --batch_size 12 \
 --num_layers 18 \
---eval_split odom_9 
+--eval_split odom_9 \
+# --jpg \
 
